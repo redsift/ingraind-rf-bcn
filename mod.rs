1e84ccd126a352e95a5175e4cf4cc0199fdff434
Ben Evans <nebopolis@gmail.com>
2019-11-09 17:14:53 +0100

    Update mod.rs
    logging

--- mod.rs
+++ mod.rs
@@ -1,12 +1,30 @@
+
 use cty::*;
-​
-// This is where you should define the types shared by the kernel and user
-// space, eg:
-//
-#[repr(C)]
-#[derive(Debug)]
+
 pub struct HTTPBlocked {
     pub saddr: u32,
     pub daddr: u32,
     pub sport: u32,
+    pub header: [u8; 16],
 }
+
+
+#[cfg(not(feature = "probes"))]
+impl HTTPBlocked {
+    pub fn data(&self) -> &[u8] {
+        unsafe {
+            let base = self.data.as_ptr().add(self.offset as usize);
+            core::slice::from_raw_parts(base, self.size as usize)
+        }
+    }
+}
+
+// This is where you should define the types shared by the kernel and user
+// space, eg:
+//
+// #[repr(C)]
+// #[derive(Debug)]
+// pub struct SomeEvent {
+//     pub pid: c_ulonglong,
+//     ...
+// }
